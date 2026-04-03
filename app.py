d1a2f25553f2b2e89382c41c8dc9676a8daf5a4b
sridattaga <sivag.soc95@gmail.com>
2026-04-03 16:04:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter('app_requests_total', 'Total Request Count')
 
 @app.route("/")
 def home():
@@ -9,6 +12,10 @@ def home():
 @app.route("/health")
 def health():
     return {"status": "running"}
+
+@app.route('/metrics')
+def metrics():
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
